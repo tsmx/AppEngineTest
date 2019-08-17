8262bce2bcd42f2be575882ec43755e2b0223f88
tsmx <strs@freenet.de>
2019-08-17 18:54:27 +0200

    Initial commit.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const logger = require('./utils/logging').logger;
+
+app.get('/', (req, res) => {
+    logger.info('Reqest received from: ' + req.ip);
+    res.status(200).json({ info: "Hello world from gcloud AppEngine!", version: "1.2" });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => { logger.info('AppEngineTest running on port ' + port); });
---
+++ utils/logging.js
@@ -0,0 +1,16 @@
+var winston = require('winston');
+var format = winston.format;
+
+const myFormat = format.printf((info) => {
+    return `${info.timestamp} ${info.level.toUpperCase()} ${info.message}`;
+});
+
+var logger = winston.createLogger({
+    format: format.combine(format.timestamp(), myFormat),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+// export a basic logger object
+module.exports.logger = logger;
