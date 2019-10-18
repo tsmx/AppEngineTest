c316032eb6f7574f66447d7b130cce9f5c996584
tsmx <strs@freenet.de>
2019-10-18 21:08:55 +0200

    Activated eslint and prettier.

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const logger = require('./utils/logging').logger;
 
 app.get('/', (req, res) => {
     logger.info('Reqest received from: ' + req.ip);
-    res.status(200).json({ info: 'Hello world from gcloud AppEngine!', version: '1.2' });
+    res.status(200).json({
+        info: 'Hello world from gcloud AppEngine!',
+        version: '1.2'
+    });
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => { logger.info('AppEngineTest running on port ' + port); });
+app.listen(port, () => {
+    logger.info('AppEngineTest running on port ' + port);
+});
